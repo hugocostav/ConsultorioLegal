e1d94473e53a6da1b165807035f0951af31e4e60
Hugo <hugocosta8c2@gmail.com>
2023-07-13 00:34:35 -0300

    Add project files.

---
+++ CL.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using CL.Data.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<Cliente>()
+            {
+                new Cliente() { Id = 1, Nome = "Joa do Canhao", DataNascimento = new DateTime(1980, 01, 01)},
+                new Cliente() { Id = 2, Nome = "Maria da Conceisao",DataNascimento = new DateTime(1980, 02, 02)}
+            }
+           );
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
